1a97a4e785f5631ce70be444cbdc9e8e43194b03
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2025-05-24 23:31:55 +0200

    feat(nvim): format on save

--- home/.config/nvim/lua/custom/plugins/lsp.lua
+++ home/.config/nvim/lua/custom/plugins/lsp.lua
@@ -1,19 +1,36 @@
 return {
     {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-	    {
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-		    library = {
-			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-		    },
-		},
-	    },
-	},
-	config = function()
-	    require("lspconfig").lua_ls.setup {}
-	end,
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "folke/lazydev.nvim",
+                ft = "lua",
+                opts = {
+                    library = {
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+        },
+        config = function()
+            require("lspconfig").lua_ls.setup {}
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if not client then return end
+
+                    if client.supports_method("textDocument/formatting") then
+                        -- Format buffer on save
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            buffer = args.buf,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
     },
 }

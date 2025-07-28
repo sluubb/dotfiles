871614855b8d262041ff7ff30bbd025f5a3d4145
SlubblesThuggy <65078309+SlubblesThuggy@users.noreply.github.com>
2025-07-28 20:21:11 +0200

    feat(neovim): add rust-analyzer lsp

--- home/.config/nvim/lua/custom/plugins/lsp.lua
+++ home/.config/nvim/lua/custom/plugins/lsp.lua
@@ -1,28 +1,31 @@
 return {
     {
-        "neovim/nvim-lspconfig",
+        'neovim/nvim-lspconfig',
         dependencies = {
             {
-                "folke/lazydev.nvim",
-                ft = "lua",
+                'folke/lazydev.nvim',
+                ft = 'lua',
                 opts = {
                     library = {
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
                     },
                 },
             },
         },
         config = function()
-            require("lspconfig").lua_ls.setup {}
+            local lspconfig = require('lspconfig')
 
-            vim.api.nvim_create_autocmd("LspAttach", {
+            lspconfig.lua_ls.setup {}
+            lspconfig.rust_analyzer.setup {}
+
+            vim.api.nvim_create_autocmd('LspAttach', {
                 callback = function(args)
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
                     if not client then return end
 
-                    if client.supports_method("textDocument/formatting") then
+                    if client.supports_method('textDocument/formatting') then
                         -- Format buffer on save
-                        vim.api.nvim_create_autocmd("BufWritePre", {
+                        vim.api.nvim_create_autocmd('BufWritePre', {
                             buffer = args.buf,
                             callback = function()
                                 vim.lsp.buf.format({ bufnr = args.buf, id = client.id })

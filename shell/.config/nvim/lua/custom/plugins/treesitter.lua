480497c3066d8406db3ce966f1875b8a4464d80c
sluubb <65078309+sluubb@users.noreply.github.com>
2026-03-10 12:09:36 +0100

    feat(treesitter.nvim): switch to main branch

--- shell/.config/nvim/lua/custom/plugins/treesitter.lua
+++ shell/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,24 +1,30 @@
 return {
     {
-        "nvim-treesitter/nvim-treesitter",
+        'nvim-treesitter/nvim-treesitter',
         enabled = true,
-        build = ":TSUpdate",
+        lazy = false,
+        branch = 'main',
+        build = ':TSUpdate',
         config = function()
-            require 'nvim-treesitter.configs'.setup {
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "zig" },
-                auto_install = false,
-                highlight = {
-                    enable = true,
-                    disable = function(lang, buf)
-                        local max_filesize = 100 * 1024 -- 100 KB
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        end
-                    end,
-                    additional_vim_regex_highlighting = false,
-                },
+            local parsers = {
+                'c',
+                'zig',
+                'lua',
+                'zsh',
+                'bash',
+                'python',
+                'markdown',
+                'markdown_inline',
             }
+
+            require('nvim-treesitter').install(parsers):wait(300000)
+
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = parsers,
+                callback = function()
+                    vim.treesitter.start()
+                end,
+            })
         end,
     },
 }

87334438430d9ad382c8156013f394eb781f7b47
sluubb <65078309+sluubb@users.noreply.github.com>
2026-03-28 09:50:57 +0100

    fix(pure): add unsets for linux term style

--- shell/.config/zsh/.zshrc.d/11-pure.zsh
+++ shell/.config/zsh/.zshrc.d/11-pure.zsh
@@ -5,6 +5,13 @@ if [[ "$TERM" == linux ]]; then
     export PURE_GIT_DOWN_ARROW='v'
     export PURE_GIT_UP_ARROW='^'
     export PURE_GIT_STASH_SYMBOL='*'
+else
+    unset PURE_PROMPT_SYMBOL
+    unset PURE_PROMPT_VICMD_SYMBOL
+    unset PURE_SUSPENDED_JOBS_SYMBOL
+    unset PURE_GIT_DOWN_ARROW
+    unset PURE_GIT_UP_ARROW
+    unset PURE_GIT_STASH_SYMBOL
 fi
 
 autoload -U promptinit; promptinit
